98a4f84fecd631c4c54e244cd95769959be5ad77
Dante Spencer <spencerdantel@gmail.com>
2019-07-29 12:45:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,2 +1,11 @@
 class TicTacToe
-  @board = Array.new(9, " ")
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = []
+  
+  def display_board
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    
+  
