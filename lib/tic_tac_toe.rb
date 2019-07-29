e3a8056668966620b84ada7b03981292b825b2cb
Dante Spencer <spencerdantel@gmail.com>
2019-07-29 13:07:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,7 +11,7 @@ class TicTacToe
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
   
-  def input_to_index
+  def input_to_index(position)
     position = gets
     @move = @board.index("position") -1
   end
